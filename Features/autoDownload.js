da5f6961cdca28136aed9b18b80fbd828abf7b79
Zaterz <sizaterz@gmail.com>
2022-07-02 23:00:02 +0700

    compress video tiktok

--- Features/autoDownload.js
+++ Features/autoDownload.js
@@ -1,6 +1,7 @@
 const axios = require("axios")
-const { createWriteStream, unlinkSync, readFileSync } = require("fs")
+const { createWriteStream, unlinkSync, readFileSync, existsSync } = require("fs")
 const { randomBytes } = require("crypto")
+const { spawn } = child_process
 module.exports = async data => {
 	const tiktokRegex = new RegExp(/^https?:\/\/(www\.|vm\.)?(tiktok\.com)\/?(.*)$/g)
 	if(tiktokRegex.test(data.msg)){
@@ -11,7 +12,8 @@ module.exports = async data => {
 			const resv = await axios({method: "get",
 						  url: resl.data[0].url,
 						  responseType: "stream"})
-			const id = randomBytes(3).toString("hex") + ".mp4"
+			const ID = randomBytes(3).toString("hex")
+			const id = ID + ".mp4"
 			const writer = createWriteStream("./Media/tmp/"+id)
 			resv.data.pipe(writer)
 			let error = null
@@ -21,8 +23,18 @@ module.exports = async data => {
 			})
 			writer.on("close", async() => {
 				if(error) return logger.error("gagal download video tiktok")
-				await sock.sendMessage(data.id,{video: readFileSync("./Media/tmp/"+id)},{quoted:data.message})
-				unlinkSync("./Media/tmp/"+id)
+				const cmd = "ffmpeg"
+				const argu = ["-i", "./Media/tmp/"+id, "-vcodec", "libx265", "-crf", "28", "./Media/tmp/"+ID+"c.mp4"]
+				const proc = spawn(cmd,argu)
+				proc.on("close", async () => {
+					function herr () {
+						unlinkSync(id)
+					}
+					if(!existsSync("./Media/tmp/"+ID+"c.mp4")) return
+					await sock.sendMessage(data.id,{video: readFileSync("./Media/tmp/"+ID+"c.mp4")},{quoted:data.message})
+					unlinkSync("./Media/tmp/"+id)
+					unlinkSync("./Media/tmp/"+ID+"c.mp4")
+				})
 			})
 		} catch(err){
 			logger.error(err)
